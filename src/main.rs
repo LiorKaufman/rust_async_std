d41136e03ec7d99bf0df0fc9a6d17c654672800d
lior@sightly.com <lior@sightly.com>
2023-02-28 20:21:53 -0800

    save some tutorial work

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,64 @@
-use async_std::{fs::File, io, prelude::*, task};
+#![allow(unused)]
 
-async fn read_file(path: &str) -> io::Result<String> {
-    let mut file = File::open(path).await?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).await?;
-    Ok(contents)
+extern crate async_std;
+use async_std::{
+    net::{TcpListener, ToSocketAddrs},
+    prelude::*,
+    task,
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+use async_std::{io::BufReader, net::TcpStream};
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e)
+        }
+    })
+}
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        println!("Accepting from: {}", stream.peer_addr()?);
+        let _handle = task::spawn(connection_loop(stream)); // 1
+    }
+    Ok(())
+}
+
+async fn connection_loop(stream: TcpStream) -> Result<()> {
+    let reader = BufReader::new(&stream); // 2
+    let mut lines = reader.lines();
+
+    let name = match lines.next().await {
+        // 3
+        None => Err("peer disconnected immediately")?,
+        Some(line) => line?,
+    };
+    println!("name = {}", name);
+
+    while let Some(line) = lines.next().await {
+        // 4
+        let line = line?;
+        let (dest, msg) = match line.find(':') {
+            // 5
+            None => continue,
+            Some(idx) => (&line[..idx], line[idx + 1..].trim()),
+        };
+        let dest: Vec<String> = dest
+            .split(',')
+            .map(|name| name.trim().to_string())
+            .collect();
+        let msg: String = msg.to_string();
+    }
+    Ok(())
 }
 
 fn main() {
-    let reader_task = task::spawn(async {
-        let result = read_file("src/data.csv").await;
-        match result {
-            Ok(s) => println!("{}", s),
-            Err(e) => println!("Error reading file: {:?}", e)
-        }
-    });
-    println!("Started task!");
-    task::block_on(reader_task);
-    println!("Stopped task!");
+    println!("he    ")
 }
